91d3fc0ee663e8421a875d10cd9bb1cb5f4057bc
jiyeon yoo <ji04230423@gmail.com>
2024-10-27 02:14:37 +0900

    Initial Commit

---
+++ cose354-project1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cose354-project1
+//
+//  Created by 유지연 on 10/27/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
